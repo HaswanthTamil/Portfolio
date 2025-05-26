b4ccfb96da66caa030229e4e1f68aae33fad88e7
Haswanth Tamil <haswanthtamilofficial@gmail.com>
2025-05-26 19:30:33 +0530

    Add Particles Background.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react"
 import "./styles/App.css"
+import ParticlesBG from "./components/ParticlesBG"
 
 function App() {
-  return <></>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <ParticlesBG />
+    </div>
+  )
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,23 @@
 module.exports = {
-  content: ["./src/**/*.{ts,js,jsx,tsx}", "./index.html"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "stars-zoom": "zoom 30s ease-in-out infinite",
+        "stars-twinkle": "twinkle 2s ease-in-out infinite",
+      },
+      keyframes: {
+        zoom: {
+          "0%": { backgroundSize: "100%" },
+          "50%": { backgroundSize: "120%" },
+          "100%": { backgroundSize: "100%" },
+        },
+        twinkle: {
+          "0%, 100%": { opacity: "0.9" },
+          "50%": { opacity: "0.5" },
+        },
+      },
+    },
   },
   plugins: [],
 }
