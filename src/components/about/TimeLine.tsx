386927a315170438547abd8527e0835251e032a7
ghost <ghost@devmail.com>
2025-12-02 20:34:19 +0530

    Clean up and resume update

--- src/components/about/TimeLine.tsx
+++ src/components/about/TimeLine.tsx
@@ -3,24 +3,20 @@
 import { motion } from "framer-motion"
 
 const timeline = [
-  { date: "Apr 2025", label: "Picked up Python. Finished it in 1 month flat." },
+  { date: "April 2025", label: "Picked up basic CS: Python, JS, OOPs" },
   {
     date: "May 2025",
-    label: "Dived head-first into frontend (React + Tailwind gang).",
+    label: "Dived into frontend (React/NextJS + Tailwind).",
   },
   {
-    date: "Jun Week 1",
-    label: "Flopped my first interview (learned a lot tho).",
-  },
-  {
-    date: "Jun Week 2",
+    date: "June 2025",
     label:
-      "Started learning Django with just the official docs. No courses. Built a simple blog app in 2 days.",
+      "Started learning Django.",
   },
-  { date: "Jun Week 2", label: "Took on an unpaid gig to gain real-world XP." },
-  { date: "Jun Week 3", label: "Landed my first paying client. Let's gooo." },
-  { date: "Jun Week 4", label: "Founded BytesBrush to make it official." },
-  { date: "Jul 2025", label: "Working on a paid collab with another startup." },
+  { date: "June 2025", label: "Took on an unpaid frontend project." },
+  { date: "July 2025", label: "Landed my first paying client." },
+  { date: "August 2025", label: "Started learning backend (Spring Boot, NodeJS)." },
+  { date: "November 2025", label: "Leading the development of accessbility enhancement tool."}
 ]
 
 export default function TimeLine() {
@@ -31,20 +27,22 @@ export default function TimeLine() {
         <h2 className="text-4xl text-white text-shadow font-bold mb-4">
           About Me
         </h2>
-        <p className="text-lg text-gray-100 text-shadow">
+        <br/>
+        <p className="text-lg text-gray-50 text-shadow">
           Hi! I&#39;m <span className="font-semibold text-white">Haswanth</span>{" "}
           — a frontend-focused fullstack dev who loves turning ideas into sleek,
           functional apps.
           <br />
+          <br />
           Founder of{" "}
           <span className="font-semibold text-white">BytesBrush</span>,
-          part-time freelancer, full-time uni survivor.
+          part-time freelancer and a student.
           <br />I don&#39;t just build UIs — I craft experiences that click.
         </p>
       </div>
 
       {/* Timeline */}
-      <div className="relative max-w-3xl mx-auto border-l border-gray-500 pl-6">
+      <div className="relative max-w-3xl mx-auto border-l-2 border-gray-500 pl-6">
         {timeline.map((item, index) => (
           <motion.div
             key={index}
@@ -54,11 +52,11 @@ export default function TimeLine() {
             viewport={{ once: true }}
             className="mb-10 relative"
           >
-            <div className="absolute -left-[15px] top-2 w-3 h-3 bg-white rounded-full border border-gray-600" />
-            <h4 className="text-white px-1 font-semibold text-lg">
+            <div className="absolute -left-[15px] top-2 w-2.5 h-2.5 bg-white opacity-40 rounded-full border border-gray-600" />
+            <h4 className="text-white px-1 mx-2 font-semibold text-md">
               {item.date}
             </h4>
-            <p className="text-gray-200 px-1 text-base">{item.label}</p>
+            <p className="text-gray-200 px-1 mx-2 text-base">{item.label}</p>
           </motion.div>
         ))}
       </div>
